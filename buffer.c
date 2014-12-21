ec8e8cec5b9c21473ad540e09c63f13fa8adf805
greg <gregorio.jp.mitchell@gmail.com>
2014-12-20 23:43:28 -0700

    starting to implement REPL

--- buffer.c
+++ buffer.c
@@ -35,35 +35,46 @@
 
 int main()
 {
-  //GString line; 
-  GArray* lines; // array of lines
-  lines = g_array_new(FALSE,FALSE,sizeof(GString *));
-  // can find array size with lines->len which will return a guint
-  // a guint is an unsigned int
-  
-  
+  // init curses
+  // -----------
   initscr();
   cbreak();
   curs_set(2);
   noecho();
   keypad(stdscr,true);
+  // end curses init
+
+  // setup structures
+  // ------------------
+  //GString line; 
+  int ymax,xmax;
+  getmaxyx(stdscr,ymax,xmax);
+  GArray* lines; // array of lines
+  lines = g_array_new(FALSE,FALSE,sizeof(GString *));
+  // can find array size with lines->len which will return a guint
+  // a guint is an unsigned int
+
+  int startLine = 0;
+  int finishLine = ymax;
+  
 
 
   // add text to lines
+  // -------------------
   GString* ar;
   for (int i = 0; i < 100; i++)
     {
-      ar = g_string_new("");
+      ar = g_string_new(""); // initialize string
+      // push text to string
       g_string_append_printf(ar, 
 			     "%d - lots and lots of lines flowing down the terminal\n",i);
+      // push string into array
       g_array_append_val(lines, ar);
-      refresh();
     }
 
   // display lines
-  int ymax,xmax;
-  getmaxyx(stdscr,ymax,xmax);
-  for (int i = 0; i < ymax; i++)
+  // --------------
+  for (int i = 0; i < ymax; i++) // get ith line
     { 
       move(i,0);
       // g_ptr_array_index(lines,i) contains ith line
@@ -71,6 +82,7 @@ int main()
       // store ith line as a new GString
       GString* line;
       line = g_array_index(lines,GString*,i);
+      // print jth char of ith line
       for (int j = 0; j < min(xmax,(int)line->len); j++) 
 	  wprintw(stdscr,"%c",line->str[j]);
     }
